f49c81ae4cad8d256a3f4e56af4fa52073a310a4
zzz <zzz@kvm2.(none)>
2013-05-12 03:02:23 +0800

    Update s.py.

--- s.py
+++ s.py
@@ -40,12 +40,6 @@ def handle(conn, addr):
         logger.debug("Closing socket")
         conn.close()
 
-def hdlr_counter():
-    logger = logging.getLogger("Counter")
-    while True:
-        print multiprocessing.active_children()
-        time.sleep(5)
-
 class Server(object):
     def __init__(self, hostname, port):
         self.logger = logging.getLogger("Server")
@@ -53,12 +47,12 @@ class Server(object):
         self.port = port
 
         self.logger_cnt = logging.getLogger("Counter")
-        self.proc_cnt = 0
+        self.proc_cnt = multiprocessing.Value('i', 0)
         self.intv_cnt = 10
 
     def hdlr_counter(self):
         while True:
-            self.logger_cnt.debug("Number of threads %d", self.proc_cnt)
+            self.logger_cnt.debug("Number of threads: %d", self.proc_cnt.value)
             time.sleep(self.intv_cnt)
 
     def start(self):
@@ -68,15 +62,15 @@ class Server(object):
         self.socket.listen(1)
 
         # new thread for counting the number of threads within interval
-        counter = multiprocessing.Process(target=hdlr_counter)
-        counter.daemon = True
-        counter.start()
+        #counter = multiprocessing.Process(target=self.hdlr_counter)
+        #counter.daemon = True
+        #counter.start()
 
         while True:
             conn, addr = self.socket.accept()
             self.logger.debug("Got connection")
             process = multiprocessing.Process(target=handle, args=(conn,addr))
-            self.proc_cnt = self.proc_cnt + 1
+            self.proc_cnt.value = self.proc_cnt.value + 1
             process.daemon = True
             process.start()
             self.logger.debug("Started process %r", process)
